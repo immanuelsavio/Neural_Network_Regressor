6e1c0269949f7a901a428913421e2d5b717df054
immanuelsavio <immanuelsavio@gmail.com>
2018-12-26 13:55:17 +0530

    Update nn_regressor.py

--- nn_regressor.py
+++ nn_regressor.py
@@ -7,3 +7,12 @@ from sklearn.model_selection import cross_val_score
 from sklearn.model_selection import KFold
 from sklearn.preprocessing import StandardScaler
 from sklearn.pipeline import Pipeline
+
+
+dataframe = pandas.read_csv("13-8-17.csv", delimiter=',')
+dataset = dataframe.values
+
+X = dataset[:,0:3]
+Y = dataset[:,3]
+
+print(Y)
