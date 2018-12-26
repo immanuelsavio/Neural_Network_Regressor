0594355e9c6921e7d71dec1df8c5b08642598963
immanuelsavio <immanuelsavio@gmail.com>
2018-12-26 14:02:53 +0530

    Update nn_regressor.py

--- nn_regressor.py
+++ nn_regressor.py
@@ -15,4 +15,20 @@ dataset = dataframe.values
 X = dataset[:,0:3]
 Y = dataset[:,3]
 
-print(Y)
+def baseline_model():
+	# create model
+	model = Sequential()
+	model.add(Dense(3, input_dim=3, kernel_initializer='normal', activation='relu'))
+	model.add(Dense(1, kernel_initializer='normal'))
+	# Compile model
+	model.compile(loss='mean_squared_error', optimizer='adam')
+	return model
+    
+seed = 7
+numpy.random.seed(seed)
+# evaluate model with standardized dataset
+estimator = KerasRegressor(build_fn=baseline_model, epochs=100, batch_size=5, verbose=0)
+
+kfold = KFold(n_splits=10, random_state=seed)
+results = cross_val_score(estimator, X, Y, cv=kfold)
+print("Results: %.2f (%.2f) MSE" % (results.mean(), results.std()))
